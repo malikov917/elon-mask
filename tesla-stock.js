26eceff7ca87592602be7a7daca01f8405c8def4
Konstantin Malikov <>
2021-02-14 21:10:19 +0200

    args uptd

--- tesla-stock.js
+++ tesla-stock.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 (async () => {
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: true, args: ['--headless', '--disable-gpu'] });
     const page = await browser.newPage();
     await page.goto('https://www.marketwatch.com/investing/stock/tsla');
     await page.waitForSelector('.intraday__price');
