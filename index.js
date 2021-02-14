ab8363167f3e7f2c4ecdb0ff731045c604182687
Konstantin Malikov <>
2021-02-14 22:06:23 +0200

    .

--- index.js
+++ index.js
@@ -9,11 +9,24 @@ const puppeteer = require('puppeteer');
     await page.goto('https://twitter.com/elonmusk');
     console.log('site opened');
 
-    await page.waitForSelector('[data-testid="tweet"]');
+    await page.waitForSelector('[href="/elonmusk"]');
     const title = await page.title();
+
+    await page.screenshot({ path: 'screenshot.png' });
+
     console.info(`The title is: ${title}`);
     console.log('wait For Selector finished')
 /* 
+    const browser = await puppeteer.launch({ headless: true, args: ['--headless', '--disable-gpu'] });
+    console.log('browser opened')
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 1200 })
+    console.log('new page opened')
+    await page.goto('https://twitter.com/elonmusk');
+    console.log('site opened');
+
+    await page.waitForSelector('[data-testid="tweet"]');
+
     const links = await page.evaluate(() => {
       let elements = Array.from(document.querySelectorAll('[data-testid="tweet"]'));
       let links = elements.map(x => x.querySelectorAll('a')[2].href);
