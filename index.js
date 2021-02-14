c6659723a54c4615ca68e18b7bab83a36846dfb5
Konstantin Malikov <>
2021-02-14 17:34:50 +0200

    =init commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://twitter.com/elonmusk');
+    await page.waitForSelector('[data-testid="tweet"]');
+
+    const links = await page.evaluate(() => {
+      let elements = Array.from(document.querySelectorAll('[data-testid="tweet"]'));
+      let links = elements.map(x => x.querySelectorAll('a')[2].href);
+      console.log(links)
+      return links;
+    });
+
+    console.log(links);
+
+    await browser.close();
+
+  } catch (error) {
+    console.log(error)
+  }
+})();
---
+++ tesla-stock.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://www.marketwatch.com/investing/stock/tsla');
+    await page.waitForSelector('.intraday__price');
+
+    const stockPrice = await page.evaluate(() => {
+      console.log(document.querySelector('.intraday__price').innerText);
+      return document.querySelector('.intraday__price').innerText;
+    });
+
+    console.log(stockPrice);
+
+    await browser.close();
+
+  } catch (error) {
+    console.log(error)
+  }
+})();
